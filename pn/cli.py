58b01527b115fd796a592a0276306130a3e158e9
Ana Paula Gomes <apgomes88@gmail.com>
2024-05-14 23:42:54 +0200

    Fetch number of pages and create async queries

--- pn/cli.py
+++ pn/cli.py
@@ -2,6 +2,8 @@ import asyncio
 
 import asyncclick as click
 
+from pn.sources.open_alex import establish_number_of_pages, fetch_papers
+
 
 @click.group()
 async def cli():
@@ -12,7 +14,14 @@ async def cli():
 @click.argument("query")
 async def cli(query):
     """Fetch articles from different sources using given QUERY."""
-    click.echo(f"Hello, {query}!")
+    number_of_pages = await establish_number_of_pages(query)
+    tasks = [fetch_papers(query, page) for page in range(1, number_of_pages + 1)]
+    gathered = await asyncio.gather(*tasks)
+
+    all_results = []
+    for results in gathered:
+        all_results.extend(results)
+    click.echo(f"Done. Pages: {number_of_pages} All results length: {len(all_results)}")
 
 
 def main():
