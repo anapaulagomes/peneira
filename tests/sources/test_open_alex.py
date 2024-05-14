9991d93ab2ce9604aeab3bbdf7c216c714a02a39
Ana Paula Gomes <apgomes88@gmail.com>
2024-05-14 20:54:24 +0200

    Add tests for openalex successful papers retrieval cases

--- tests/sources/test_open_alex.py
+++ tests/sources/test_open_alex.py
@@ -1,3 +1,5 @@
+import json
+
 import httpx
 import pytest
 from pn.sources.open_alex import fetch_papers
@@ -22,3 +24,25 @@ async def test_no_results_due_to_end_of_pagination(respx_mock):
     results = await fetch_papers(query)
 
     assert results == []
+
+
+@pytest.mark.respx(base_url="https://api.openalex.org")
+async def test_return_all_results_from_first_page(respx_mock):
+    payload = json.loads(open("tests/sources/fixtures/works.json").read())
+    respx_mock.get("/works").mock(return_value=httpx.Response(200, json=payload))
+    query = "machine learning AND public health"
+
+    results = await fetch_papers(query)
+
+    assert len(results) == 49
+
+
+@pytest.mark.respx(base_url="https://api.openalex.org")
+async def test_add_support_to_pagination(respx_mock):
+    payload = json.loads(open("tests/sources/fixtures/works.json").read())
+    respx_mock.get("/works").mock(return_value=httpx.Response(200, json=payload))
+    query = "machine learning AND public health"
+
+    results = await fetch_papers(query, page=10)
+
+    assert len(results) == 49
